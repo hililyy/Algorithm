aed1bc59669ddb8ec30c2ae29ce23c5ee85b9a4b
hililyy <joun406@gmail.com>
2022-05-24 23:28:09 +0900

    알고리즘 수업 - 깊이 우선 탐색 2

---
+++ 24480.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool visited[100001] = {0};
+vector <int> vec[100001];
+int output[200001] = {0};
+
+int cnt = 1;
+
+void dfs(int v) {
+    visited[v] = true;
+    output[v] = cnt++;
+    for (int node: vec[v]) {
+        if (visited[node]) continue;
+        dfs(node);
+    }
+}
+
+bool comp(int a, int b) {
+    return a > b;
+}
+
+int main() {
+    
+    cin.tie(0) -> sync_with_stdio(0);
+    cout.tie(0) -> sync_with_stdio(0);
+    int n, m, r;
+
+    cin >> n >> m >> r;
+    
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        vec[u].push_back(v);
+        vec[v].push_back(u);
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        sort(vec[i].begin(), vec[i].end(), comp);
+    }
+
+    dfs(r);
+    
+    for (int i = 1; i <= n; i++) {
+        cout << output[i] <<'\n';
+    }
+}
