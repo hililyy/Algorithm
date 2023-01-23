cea9afcf5cf01c14606ffc43553f1f5d88c6325d
hililyy <joun406@gmail.com>
2023-01-23 22:45:02 +0900

    퇴사

--- baekjoon/14501.cpp
+++ baekjoon/14501.cpp
@@ -1,3 +1,271 @@
+Last login: Wed Dec 28 22:11:57 on ttys000
+The `osx` plugin is deprecated and has been renamed to `macos`.
+Please update your .zshrc to use the `macos` plugin instead.
+ lily 🐤  ~
+ ls
+ Applications   Movies                     강조은 이력서(한글).hwp
+ boostcourse    Music                      강조은_교재유출금지서약서.jpeg
+ Desktop        Pictures                   무제 폴더
+ Documents      Postman                    증명서
+ Downloads      Public                     콜라보 멘토링 문제.pdf
+ iOS            ~$은 자기소개서.docx
+ Library        강조은 이력서(워드).docx
+ lily 🐤  ~
+ cd iOS
+ lily 🐤  ~/iOS
+ ls
+ algorithm-study   ios-pre-onboarding       SwiftMaster
+ appSeminar        iOSInterviewquestions    XcodeClone
+ Doit              Projects                 yagomAcademy
+ fastcampus        socar coding interview
+ lily 🐤  ~/iOS
+ cd XcodeClone
+ lily 🐤  ~/iOS/XcodeClone
+ ls
+ clock_clone     dtvtn-ios                     oview_w_ios
+ dnd-7th-1-iOS   iOS-Clean-Architecture-MVVM
+ lily 🐤  ~/iOS/XcodeClone
+ git clone https://github.com/taewoojin/CollectionViewPagingExample.git
+Cloning into 'CollectionViewPagingExample'...
+remote: Enumerating objects: 52, done.
+remote: Total 52 (delta 0), reused 0 (delta 0), pack-reused 52
+Receiving objects: 100% (52/52), 34.51 KiB | 175.00 KiB/s, done.
+Resolving deltas: 100% (14/14), done.
+ lily 🐤  ~/iOS/XcodeClone
+ cd CollectionViewPagingExample
+ lily 🐤  ~/iOS/XcodeClone/CollectionViewPagingExample   master
+ open .
+ lily 🐤  ~/iOS/XcodeClone/CollectionViewPagingExample   master
+ cd ..
+ lily 🐤  ~/iOS/XcodeClone
+ cd ..
+ lily 🐤  ~/iOS
+ ls
+ algorithm-study   ios-pre-onboarding       SwiftMaster
+ appSeminar        iOSInterviewquestions    XcodeClone
+ Doit              Projects                 yagomAcademy
+ fastcampus        socar coding interview
+ lily 🐤  ~/iOS
+ cd Projects/DevTodoNote
+ lily 🐤  ~/iOS/Projects/DevTodoNote   feature/signin ±✚
+ ls
+ DevTodoNote             DevTodoNote.xcworkspace   Podfile.lock
+ DevTodoNote.xcodeproj   Podfile                   Pods
+ lily 🐤  ~/iOS/Projects/DevTodoNote   feature/signin ±✚
+ open .
+ lily 🐤  ~/iOS/Projects/DevTodoNote   feature/signin ±✚
+ open Podfile
+ lily 🐤  ~/iOS/Projects/DevTodoNote   feature/signin ±✚
+ pod install --repo-update
+Updating local specs repositories
+Analyzing dependencies
+Downloading dependencies
+Installing Pageboy (4.0.2)
+Installing Tabman (3.0.1)
+Generating Pods project
+Integrating client project
+Pod installation complete! There are 6 dependencies from the Podfile and 8 total pods installed.
+
+[!] Automatically assigning platform `iOS` with version `14.0` on target `DevTodoNote` because no platform was specified. Please specify a platform for this target in your Podfile. See `https://guides.cocoapods.org/syntax/podfile.html#platform`.
+ lily 🐤  ~/iOS/Projects/DevTodoNote   feature/signin ±✚
+ open .
+ lily 🐤  ~/iOS/Projects/DevTodoNote   feature/signin ±✚
+ ls
+ DevTodoNote             DevTodoNote.xcworkspace   Podfile.lock
+ DevTodoNote.xcodeproj   Podfile                   Pods
+ lily 🐤  ~/iOS/Projects/DevTodoNote   feature/signin ±✚
+ cd ..
+ lily 🐤  ~/iOS/Projects
+ cd ..
+ lily 🐤  ~/iOS
+ cd ..
+ lily 🐤  ~
+ ls
+ Applications   Movies                     강조은 이력서(한글).hwp
+ boostcourse    Music                      강조은_교재유출금지서약서.jpeg
+ Desktop        Pictures                   무제 폴더
+ Documents      Postman                    증명서
+ Downloads      Public                     콜라보 멘토링 문제.pdf
+ iOS            ~$은 자기소개서.docx
+ Library        강조은 이력서(워드).docx
+ lily 🐤  ~
+ cd iOS/Projects/Thanks\ Diary
+ lily 🐤  ~/iOS/Projects/Thanks Diary   develop ±
+ cd ../../../
+ lily 🐤  ~
+ ls
+ Applications   Movies                     강조은 이력서(한글).hwp
+ boostcourse    Music                      강조은_교재유출금지서약서.jpeg
+ Desktop        Pictures                   무제 폴더
+ Documents      Postman                    증명서
+ Downloads      Public                     콜라보 멘토링 문제.pdf
+ iOS            ~$은 자기소개서.docx
+ Library        강조은 이력서(워드).docx
+ lily 🐤  ~
+ cd -
+~/iOS/Projects/Thanks Diary
+ lily 🐤  ~/iOS/Projects/Thanks Diary   develop ±
+ cd ~
+ lily 🐤  ~
+ ls
+ Applications   Movies                     강조은 이력서(한글).hwp
+ boostcourse    Music                      강조은_교재유출금지서약서.jpeg
+ Desktop        Pictures                   무제 폴더
+ Documents      Postman                    증명서
+ Downloads      Public                     콜라보 멘토링 문제.pdf
+ iOS            ~$은 자기소개서.docx
+ Library        강조은 이력서(워드).docx
+ lily 🐤  ~
+ cd ..
+ lily 🐤  /Users
+ ls
+ lily   Shared
+ lily 🐤  /Users
+ cd ../
+ lily 🐤  /
+ cd ..
+ lily 🐤  /
+ cd Users/lily/iOS/Projects/Thanks\ Diary/
+ lily 🐤  ~/iOS/Projects/Thanks Diary   develop ±
+ cd ..
+ lily 🐤  ~/iOS/Projects
+ cd ..
+ lily 🐤  ~/iOS
+ ls
+ algorithm-study   ios-pre-onboarding       SwiftMaster
+ appSeminar        iOSInterviewquestions    XcodeClone
+ Doit              Projects                 yagomAcademy
+ fastcampus        socar coding interview
+ lily 🐤  ~/iOS
+ rm -rf algorithm-study/
+ lily 🐤  ~/iOS
+ ls
+ appSeminar   ios-pre-onboarding      socar coding interview   yagomAcademy
+ Doit         iOSInterviewquestions   SwiftMaster
+ fastcampus   Projects                XcodeClone
+ lily 🐤  ~/iOS
+ git clone https://github.com/hililyy/algorithm-study.git
+Cloning into 'algorithm-study'...
+remote: Enumerating objects: 286, done.
+    if (a.second == b.second) {
+remote: Counting objects: 100% (111/111), done.
+remote: Compressing objects: 100% (89/89), done.
+remote: Total 286 (delta 40), reused 82 (delta 19), pack-reused 175
+Receiving objects: 100% (286/286), 47.95 KiB | 570.00 KiB/s, done.
+Resolving deltas: 100% (105/105), done.
+ lily 🐤  ~/iOS
+ cd algorithm-study
+ lily 🐤  ~/iOS/algorithm-study   main
+ cd baekjoon
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ ls
+ 1011.cpp    10845.cpp   13417.cpp   1789.cpp    2606.cpp   4949.cpp
+ 1012.cpp    11047.cpp   13458.cpp   1904        2667.cpp   7568.py
+ 1018.cpp    11399.cpp   1436.cpp    1920.cpp    2775.cpp   9012.cpp
+ 10250.cpp   1149.cpp    1439.cpp    1929.cpp    2798.cpp   9095.cpp
+ 1026.cpp    11659.cpp   1463.cpp    1931.cpp    2839.cpp   9461.cpp
+ 1049.cpp    11724.cpp   14729.cpp   2141        2847.cpp   9498.cpp
+ 10610.cpp   11726.cpp   1541.cpp    24444.cpp   2869.cpp   README.md
+ 10757.cpp   1193.cpp    1546.cpp    24445.cpp   2941.cpp
+ 10773.cpp   1260.cpp    1673.cpp    24479.cpp   3036.cpp
+ 10816.cpp   1292.cpp    16953       24480.cpp   3986.cpp
+ 10828.cpp   13410.cpp   1744.cpp    2559.cpp    4796.cpp
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ vim 11651.cpp
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ git commit -m "좌표 정렬하기2"
+On branch main
+Your branch is up to date with 'origin/main'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	11651.cpp
+
+nothing added to commit but untracked files present (use "git add" to track)
+ ✘ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ git add .
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main ✚
+ git commit -m "좌표 정렬하기2"
+[main 7d50ced] 좌표 정렬하기2
+ 1 file changed, 29 insertions(+)
+ create mode 100644 baekjoon/11651.cpp
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ git push origin main
+int dp[41] = {0, 1};
+Enumerating objects: 6, done.
+Counting objects: 100% (6/6), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (4/4), 697 bytes | 697.00 KiB/s, done.
+Total 4 (delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/hililyy/algorithm-study.git
+   2540b8f..7d50ced  main -> main
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ ls
+ 1011.cpp    10845.cpp   13410.cpp   1744.cpp    2559.cpp   4796.cpp
+ 1012.cpp    11047.cpp   13417.cpp   1789.cpp    2606.cpp   4949.cpp
+ 1018.cpp    11399.cpp   13458.cpp   1904        2667.cpp   7568.py
+ 10250.cpp   1149.cpp    1436.cpp    1920.cpp    2775.cpp   9012.cpp
+ 1026.cpp    11651.cpp   1439.cpp    1929.cpp    2798.cpp   9095.cpp
+ 1049.cpp    11659.cpp   1463.cpp    1931.cpp    2839.cpp   9461.cpp
+ 10610.cpp   11724.cpp   14729.cpp   2141        2847.cpp   9498.cpp
+#include <iostream>
+ 10757.cpp   11726.cpp   1541.cpp    24444.cpp   2869.cpp   README.md
+ 10773.cpp   1193.cpp    1546.cpp    24445.cpp   2941.cpp
+ 10816.cpp   1260.cpp    1673.cpp    24479.cpp   3036.cpp
+ 10828.cpp   1292.cpp    16953       24480.cpp   3986.cpp
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ vim 1003.cpp
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ vim 1003(1).cpp
+zsh: no matches found: 1003(1).cpp
+ ✘ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ ls
+ 1003.cpp    10828.cpp   1292.cpp    16953       24480.cpp   3986.cpp
+ 1011.cpp    10845.cpp   13410.cpp   1744.cpp    2559.cpp    4796.cpp
+ 1012.cpp    11047.cpp   13417.cpp   1789.cpp    2606.cpp    4949.cpp
+ 1018.cpp    11399.cpp   13458.cpp   1904        2667.cpp    7568.py
+ 10250.cpp   1149.cpp    1436.cpp    1920.cpp    2775.cpp    9012.cpp
+ 1026.cpp    11651.cpp   1439.cpp    1929.cpp    2798.cpp    9095.cpp
+ 1049.cpp    11659.cpp   1463.cpp    1931.cpp    2839.cpp    9461.cpp
+ 10610.cpp   11724.cpp   14729.cpp   2141        2847.cpp    9498.cpp
+ 10757.cpp   11726.cpp   1541.cpp    24444.cpp   2869.cpp    README.md
+ 10773.cpp   1193.cpp    1546.cpp    24445.cpp   2941.cpp
+ 10816.cpp   1260.cpp    1673.cpp    24479.cpp   3036.cpp
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ vim 1003-1.cpp
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ git add .
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main ✚
+ git commit -m "피보나치 함수(dp)"
+[main 18bfaf4] 피보나치 함수(dp)
+ 2 files changed, 47 insertions(+)
+ create mode 100644 baekjoon/1003-1.cpp
+ create mode 100644 baekjoon/1003.cpp
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ git push origin main
+#include <iostream>
+Enumerating objects: 7, done.
+Counting objects: 100% (7/7), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (5/5), done.
+Writing objects: 100% (5/5), 830 bytes | 830.00 KiB/s, done.
+Total 5 (delta 2), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (2/2), completed with 1 local object.
+To https://github.com/hililyy/algorithm-study.git
+   7d50ced..18bfaf4  main -> main
+ lily 🐤  ~/iOS/algorithm-study/baekjoon   main
+ ls
+ 1003-1.cpp   10816.cpp   1260.cpp    1673.cpp    24479.cpp   3036.cpp
+ 1003.cpp     10828.cpp   1292.cpp    16953       24480.cpp   3986.cpp
+ 1011.cpp     10845.cpp   13410.cpp   1744.cpp    2559.cpp    4796.cpp
+ 1012.cpp     11047.cpp   13417.cpp   1789.cpp    2606.cpp    4949.cpp
+ 1018.cpp     11399.cpp   13458.cpp   1904        2667.cpp    7568.py
+ 10250.cpp    1149.cpp    1436.cpp    1920.cpp    2775.cpp    9012.cpp
+ 1026.cpp     11651.cpp   1439.cpp    1929.cpp    2798.cpp    9095.cpp
+ 1049.cpp     11659.cpp   1463.cpp    1931.cpp    2839.cpp    9461.cpp
+ 10610.cpp    11724.cpp   14729.cpp   2141        2847.cpp    9498.cpp
 #include <iostream>
 #include <math.h>
 using namespace std;
@@ -8,11 +276,11 @@ int main() {
     int p[1001] = {0};
     int dp[1001] = {0};
     cin >> n;
-    
+
     for(int i = 1; i <= n; i++) {
         cin >> t[i] >> p[i];
     }
-    
+
     for(int i = n; i > 0; i--) {
         if(t[i] > n - i + 1) dp[i] = dp[i+1];
         else {
@@ -22,3 +290,4 @@ int main() {
     cout << dp[1];
 }
 
+-- INSERT --
