647aeacd3893056e13d2aa8e3eba4d77cb3ee0a3
Lily <76806444+hililyy@users.noreply.github.com>
2023-04-10 23:02:54 +0900

    [unrated] Title: 귤 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ k:Int, _ tangerine:[Int]) -> Int {
+    var dic: [Int : Int] = [:]
+    var result: Int = 0
+    var _k = k
+    for i in tangerine {
+        if let num = dic[i] {
+            dic[i]! += 1
+        } else {
+            dic[i] = 1
+        }
+    }
+    let newDic = dic.sorted {
+        return $0.1 > $1.1
+    }
+    
+    for (_, value) in newDic {
+        if _k != 0 {
+            result += 1
+            _k = _k-value
+        }
+        if _k <= 0 {
+            break
+        }
+    }
+    return result
+}
