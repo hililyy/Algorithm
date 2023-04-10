16ef68dc909a912088889a2b1d84b850b943f9ff
Lily <76806444+hililyy@users.noreply.github.com>
2023-04-10 23:04:52 +0900

    Update 귤 고르기.swift

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.swift
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.swift
@@ -4,6 +4,7 @@ func solution(_ k:Int, _ tangerine:[Int]) -> Int {
     var dic: [Int : Int] = [:]
     var result: Int = 0
     var _k = k
+    
     for i in tangerine {
         if let num = dic[i] {
             dic[i]! += 1
@@ -11,6 +12,7 @@ func solution(_ k:Int, _ tangerine:[Int]) -> Int {
             dic[i] = 1
         }
     }
+    
     let newDic = dic.sorted {
         return $0.1 > $1.1
     }
@@ -18,7 +20,7 @@ func solution(_ k:Int, _ tangerine:[Int]) -> Int {
     for (_, value) in newDic {
         if _k != 0 {
             result += 1
-            _k = _k-value
+            _k = _k - value
         }
         if _k <= 0 {
             break
