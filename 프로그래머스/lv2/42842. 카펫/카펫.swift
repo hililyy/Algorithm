d3101cf08bc097ff942e299f5af62bdcb00b94ec
Lily <76806444+hililyy@users.noreply.github.com>
2023-05-10 19:12:35 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.swift
+++ 프로그래머스/lv2/42842. 카펫/카펫.swift
@@ -2,12 +2,9 @@ import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
     for x in 1...yellow {
-        var y = 0
-        if yellow % x == 0 {
-           y = yellow / x
-        } else {
-            continue
-        }
+        var y = yellow / x
+        if yellow % x != 0 { continue }
+        
         if (x * 2) + (y * 2) + 4 == brown {
             return [max(x + 2, y + 2), min(x + 2, y + 2)]
         }
