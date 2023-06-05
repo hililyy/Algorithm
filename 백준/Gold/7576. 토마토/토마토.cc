f524f9ad2c90ae4c2413783ed5a4009ddff777ff
Lily <76806444+hililyy@users.noreply.github.com>
2023-06-05 22:46:21 +0900

    [Gold V] Title: 토마토, Time: 84 ms, Memory: 11192 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+bool is_visited[1001][1001] = { 0 };
+int field[1001][1001] = { 0 };
+queue<pair<int, int>> q;
+int x_arr[4] = {0 ,0, -1, 1};
+int y_arr[4] = {1, -1, 0, 0};
+int max_day = 0;
+
+void bfs() {
+    while(!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        
+        for(int t = 0; t < 4; t++) {
+            int xx = x + x_arr[t];
+            int yy = y + y_arr[t];
+            
+            if(xx < 0 || yy < 0 || xx >= m || yy >= n) continue;
+            if(!is_visited[xx][yy] && field[xx][yy] == 0) {
+                is_visited[xx][yy] = true;
+                field[xx][yy] = field[x][y] + 1;
+                q.push(make_pair(xx, yy));
+            }
+        }
+    }
+}
+
+int main() {
+    cout.tie(0); cin.tie(0) -> sync_with_stdio(0);
+    cin >> n >> m;
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> field[i][j];
+            if(field[i][j] == 1) {
+                q.push(make_pair(i, j));
+                is_visited[i][j] = true;
+            }
+        }
+    }
+    
+    bfs();
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(field[i][j] == 0) { 
+                cout << "-1"; 
+                return 0;
+            }
+            max_day = max(max_day, field[i][j]);
+        }
+    }
+    
+    if(max_day == 1) { 
+        cout << "0"; 
+        return 0; 
+    }
+     cout << max_day - 1;
+     return 0;
+}
