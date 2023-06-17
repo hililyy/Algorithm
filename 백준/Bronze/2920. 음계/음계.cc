5a8273b5afff6426954b2d1c95ae23537e7b0130
Lily <76806444+hililyy@users.noreply.github.com>
2023-06-17 22:01:56 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+    
+    if (str == "1 2 3 4 5 6 7 8") {
+        cout << "ascending";
+    } else if (str == "8 7 6 5 4 3 2 1") {
+        cout << "descending";
+    } else {
+        cout << "mixed";
+    }
+    return 0;
+}
