77fc5869d45a4c7f26f0c11b53dd2ec8511ffb38
Lily <76806444+hililyy@users.noreply.github.com>
2023-07-04 21:49:07 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()  {
+  int num;
+  string str;
+  cin >> num;
+  
+  for(int i=0;i<num;i++) {
+      cin >> str;
+      cout << str[0] << str[str.length() - 1] << '\n';
+  }
+  
+  return 0;
+}
