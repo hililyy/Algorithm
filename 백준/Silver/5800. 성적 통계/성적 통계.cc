d480b9d84c3a529f7fed51f49a4870407cb69733
Lily <76806444+hililyy@users.noreply.github.com>
2023-07-19 17:19:47 +0900

    [Silver V] Title: 성적 통계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/5800. 성적 통계/성적 통계.cc
+++ 백준/Silver/5800. 성적 통계/성적 통계.cc
@@ -9,18 +9,18 @@ int main()  {
   int t;
   int arr[51] = { 0 };
   cin >> t;
+  
   for(int i = 1; i <= t; i++) {
     int n;
     cin >> n;
+    
     for(int j = 0; j < n; j++) cin >> arr[j];
     
     sort(arr, arr + n);
     
     int largest = 0;
     
-    for(int k = 1; k < n; k++) {
-      largest = max(largest, arr[k] - arr[k-1]);
-    }
+    for(int k = 1; k < n; k++) largest = max(largest, arr[k] - arr[k-1]);
     
     cout << "Class "<< i <<'\n';
     cout << "Max "<< arr[n - 1] << ", Min " << arr[0] << ", Largest gap " << largest << '\n';
