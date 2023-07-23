fac1f17fcf28bc43d93e648ddb032d282e60f95b
Lily <76806444+hililyy@users.noreply.github.com>
2023-07-23 23:08:04 +0900

    [Silver III] Title: 프린터 큐, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.cc
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cc
@@ -7,49 +7,44 @@ int main() {
   int t;
   cin >> t;
   
-  for(int i=0;i<t;i++) {
+  for(int i = 0; i < t; i++) {
     queue<pair<int, int>> q;
     int count = 0;
     int docs[10] = {0};
     int n, target, target_num;
     bool flag = true;
+    
     cin >> n >> target;
+    
     for(int j = 0; j < n; j++) {
       int doc; // 문서 중요도
       cin >> doc;
       
       if(target == j) target_num = doc;
-      // cout << "target_num: " << target_num << endl;
       docs[doc]++;
       q.push({doc, j}); // 문서 중요도, id
     }
-    // cout << "1: " << q.front().first << "  2: " << q.front().second << " docs[5]: " << docs[5] << endl;
     
-    // for(int k=0;k<10;i++) {
+    for(int k = 9; k > 0; k--) {
+      int now; // 현재 가장 중요한 문서의 중요도 
       
-    // }
-    
-    for(int k=9;k>0;k--) {
-      int now;
       if(docs[k]) {
         now = k;
-        // cout << "now: " << now << endl;
         docs[k]--;
         k++;
       } else continue; // 현재 가장 중요한 문서의 중요도를 구함
-      // cout << "1: " << q.front().first << "  2: " << now << endl;
+      
       while(flag) {
-        if(q.front().first < now) {
+        if(q.front().first < now) { // 뒤에 더 중요한 문서가 있는 경우
           pair<int, int> front = q.front();
-          // cout << "1: " << front.first << "  2: " << front.second << endl;
           q.pop();
           q.push(front);
-        } else if(now==target_num && q.front().second == target) {
+        } else if(now == target_num && q.front().second == target) { // 찾으려는 문서인 경우 (id 일치)
           count++;
           cout << count << endl;
           flag = false;
           break;
-        } else if(now!=target_num && q.front().first >= now) {
+        } else if(now != target_num && q.front().first >= now) { // 찾으려는 문서의 중요도만 같은 경우 (id는 불일치) 
           q.pop();
           count++;
           break;
@@ -62,11 +57,3 @@ int main() {
   }
   return 0;
 }
-// (1) 1 9 1 1 1
-// 1 9 1 1 1 (1)
-// 9 1 1 1 (1) 1 
-// 1 1 1 (1) 1 // 1
-// 1 1 (1) 1 // 2
-// 1 (1) 1 // 3
-// (1) 1 // 4
-// // 5
