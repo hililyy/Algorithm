634c2573cfc460d9772a48c3bbbeb7304c69957c
Lily <76806444+hililyy@users.noreply.github.com>
2023-09-07 12:22:27 +0900

    [구름톤] 15일차 미션(개선)

--- groomton/mission-day15.cpp
+++ groomton/mission-day15.cpp
@@ -3,33 +3,37 @@ typedef long long ll;
 using namespace std;
 
 int N, K;
-ll answer;
 vector<pair<int, ll>> fruits;
 
-int main() {
-	cout.tie(0); cin.tie(0) -> sync_with_stdio(0);
-
-	cin >> N >> K;
-	
-	for(int i = 1; i <= N; i++) {
-		int P, C;
-		cin >> P >> C;
-		fruits.push_back({C / P, P});
-	}
-	
-	sort(fruits.begin(), fruits.end());
+ll solution() {
+	ll answer = 0;
 	
 	while(N--) {
 		if(K >= fruits[N].second) {
-			K = K - fruits[N].second;
+			K -= fruits[N].second;
 			answer += fruits[N].first  * fruits[N].second;
 		} else {
 			answer += fruits[N].first * K;
 			break;
 		}
 	}
+	return answer;
+}
+
+int main() {
+	cout.tie(0); cin.tie(0) -> sync_with_stdio(0);
+
+	cin >> N >> K;
 	
-	cout << answer;
+	for(int i = 0; i < N; i++) {
+		int P, C;
+		cin >> P >> C;
+		fruits.push_back({C / P, P});
+	}
+	
+	sort(fruits.begin(), fruits.end());
+
+	cout << solution();
 	
 	return 0;
 }
