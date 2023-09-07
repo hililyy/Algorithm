a7d3a35019a9365dedd531f2070e7c8df77f692c
Lily <76806444+hililyy@users.noreply.github.com>
2023-09-07 12:06:44 +0900

    [구름톤] 14일차 미션(개선)

--- groomton/mission-day14.cpp
+++ groomton/mission-day14.cpp
@@ -1,16 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, K, answer;
+int N, M, K;
 bool is_visited[2001], flag;
 vector<int> v[2001];
+
+pair<int, int> solution() {
+	int answer = 0;
+	
+	while(!flag) {
+		answer++;
+		flag = true;
+		int _min = N;
+		
+		for(int next: v[K]) {
+			if(is_visited[next]) continue;
+			_min = min(_min, next);
+			flag = false;
+		}
+		
+		if(flag) break;
+		
+		is_visited[_min] = true;
+		K = _min;
+	}
+	
+	return {answer, K};
+}
 
 int main() {
 	cout.tie(0); cin.tie(0) -> sync_with_stdio(0);
 	
 	cin >> N >> M >> K;
 	
-	for(int i = 0; i < M; i++) {
+	while(M--) {
 		int a, b;
 		cin >> a >> b;
 		v[a].push_back(b);
@@ -19,24 +42,9 @@ int main() {
 	
 	is_visited[K] = true;
 	
-	while(!flag) {
-		answer++;
-		flag = true;
-		int minium = N;
-		
-		for(int next: v[K]) {
-			if(is_visited[next]) continue;
-			flag = false;
-			minium = min(minium, next);
-		}
-		
-		if(flag) break;
-		
-		is_visited[minium] = true;
-		K = minium;
-	}
+	auto answer = solution();
 	
-	cout << answer << ' ' << K;
+	cout << answer.first << ' ' << answer.second;
 	
 	return 0;
 }
