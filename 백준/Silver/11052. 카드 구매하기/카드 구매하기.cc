729a2adb22a2280a6dd5dc3d3d77f2b890a31c37
Lily <joun406@gmail.com>
2023-12-12 13:32:51 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int n, card, dp[10001];
+int n, dp[10001];
 
 int main() {
   cout.tie(0); cin.tie(0) -> sync_with_stdio(0);
   
   cin >> n;
-  cin >> dp[1];
   
-  for(int i = 2; i <= n; i++) {
+  for(int i = 1; i <= n; i++) {
     cin >> dp[i];
     
-    for(int j = i-1; j >= 1; j--) {
+    for(int j = i - 1; j >= i / 2; j--) {
       dp[i] = max(dp[i], dp[j] + dp[i - j]);
     }
   }
