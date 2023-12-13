89a76b35e5f05833cc67faf8bb1d33a53c23b531
Lily <joun406@gmail.com>
2023-12-13 13:50:29 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -15,13 +15,11 @@ int main() {
       if(j == 0) dp[i][0] = dp[i - 1][1];
       else if(j == 9) dp[i][9] = dp[i - 1][8];
       else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
-      
     }
   }
   
   for(int i = 0; i <= 9; i++) answer += dp[n][i];
-  
-  
+
   cout << answer % 1000000000;
   
   return 0;
