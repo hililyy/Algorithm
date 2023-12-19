d869caee61e82f582b93270e86645b0ef9483f2c
Lily <joun406@gmail.com>
2023-12-19 13:27:44 +0900

    [Gold III] Title: 내리막 길, Time: 100 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,28 +1,26 @@
 #include <iostream>
 using namespace std;
-int m, n, field[501][501], dp[501][501];
+int m, n, field[501][501], dp[501][501]; // dp: i,j번째에서 목적지까지 가는 방법 개수 저장
 int wx[] = {-1, 1, 0, 0};
 int wy[] = {0, 0, -1, 1};
 
 int dfs(int y, int x) {
-  if(y == m && x == n) return 1;
+  if(y == m && x == n) return 1; // 목적지 -> 목적지까지 가는 길은 1개
   
-  if(dp[y][x] != -1) return dp[y][x];
-  dp[y][x] = 0;
+  if(dp[y][x] == -1) {
+    dp[y][x] = 0;
   
-  for(int i = 0; i < 4; i++) {
-    int yy = wy[i] + y;
-    int xx = wx[i] + x;
-    // cout << "xx: " << xx << " yy: " << yy << endl;
-    
-    if(yy < 1 || yy > m || xx < 1 || xx > n) continue;
-    if(field[y][x] > field[yy][xx]) {
-      // cout << "1 : " << field[y][x] << " " << " 2 : " << field[yy][xx] << endl; 
-      int answer = dfs(yy, xx);
-      // cout << "y: " << y << " x: " << x << " a: " << answer << endl;
-      dp[y][x] += answer;
+    for(int i = 0; i < 4; i++) {
+      int yy = wy[i] + y;
+      int xx = wx[i] + x;
+      
+      if(yy < 1 || yy > m || xx < 1 || xx > n) continue;
+      if(field[y][x] > field[yy][xx]) {
+        dp[y][x] += dfs(yy, xx); // 목적 지점을 발견하면 돌아오면서 개수를 더함
+      }
     }
   }
+  
   return dp[y][x];
 }
 
@@ -30,21 +28,14 @@ int main() {
   
   cin >> m >> n;
   
-  for(int i=1;i<=m;i++){ // i -> 세로
-    for(int j=1;j<=n;j++) { // j -> 가로
+  for(int i = 1; i <= m; i++){ // i -> 세로
+    for(int j = 1; j <= n; j++) { // j -> 가로
       cin >> field[i][j];
       dp[i][j] = -1;
     }
   }
   
- cout << dfs(1, 1)<<endl;
-  
-  // for(int i = 1; i <= m; i++){ // i -> 세로
-  //   for(int j = 1; j <= n; j++) { // j -> 가로
-  //     cout << dp[i][j] << ' ';
-  //   }
-  //   cout << endl;
-  // }
-  
+ cout << dfs(1, 1); // 시작지점 ~ 끝 지점 까지의 거리를 구해야함
+ 
   return 0;
 }
